2d4647057e6a0018406389ef96dbb10a0eabce09
Ryo Otwell <ytryo789@gmail.com>
2024-05-27 01:20:30 +0800

    + github footer

--- src/app/page.js
+++ src/app/page.js
@@ -71,10 +71,10 @@ function Home() {
                         <div>
                             <div className="lg:inline-flex lg:items-center">
                                 <h1 className='text-4xl font-bold tracking-tight dark:text-slate-300 text-gray-900 uppercase mr-2'>
-                                    Monitoring
+                                    Monitoring Kualitas
                                 </h1>
                                 <span className="mt-2 inline-block -rotate-1 animate-gradient-pulse rounded-xl bg-gradient-to-r from-background via-background to-background px-4 py-1.5 text-lg tracking-tight text-foreground shadow-2xl shadow-primary/[0.25] ring-2 ring-primary/70 dark:via-primary/10 sm:px-4 sm:py-3 sm:text-3xl lg:text-4xl">
-                                    Tanaman
+                                    Udara
                                 </span>
                             </div>
                             <p className="max-w-sm my-4 dark:text-slate-300/60 text-slate-900/60">
@@ -97,7 +97,7 @@ function Home() {
 
                 <div className="text-center px-10 py-16 flex justify-center">
                     <div>
-                        <h2 className="text-4xl font-bold tracking-tight dark:text-slate-300 text-gray-900">Monitoring Tananam Secara<br />Real Time.</h2>
+                        <h2 className="text-4xl font-bold tracking-tight dark:text-slate-300 text-gray-900">Monitoring Kualitas Udara Secara<br />Real Time.</h2>
                         <p className="my-4 text-slate-950/60 dark:text-slate-300/60">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, necessitatibus.
                         </p>
@@ -152,8 +152,8 @@ function Home() {
                     </div>
                 </div>
 
-                <div className="py-10 text-sm text-center px-10 dark:text-slate-300 text-slate-900/60">
-                    full-stack kok PeHaPe d0ang
+                <div className="py-10 text-sm text-center px-10 dark:text-slate-300 text-slate-900/60 flex justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
                 </div>
             </div>
         </>
