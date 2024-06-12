556eb2ca950e00707feb74aa33dbb6f4dcd07851
Ryo Otwell <ytryo789@gmail.com>
2024-06-13 02:30:32 +0800

    fix never use

--- src/components/TimeCounter.js
+++ src/components/TimeCounter.js
@@ -1,5 +1,5 @@
 import { formatTime } from "@/lib/utils"
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 function TimeCounter({ createdAt }) {
     const [elapsedTime, setElapsedTime] = useState(0)
